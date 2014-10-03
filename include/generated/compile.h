9942fc779816df4e85c3d4e096a3dadc8df3a7b7
bauner <bauner@gmx.de>
2014-10-03 17:33:27 +0200

    initial upload

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Tue Dec 18 11:14:22 CST 2012"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "android116-desktop"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
